f7c3e6428c75a4fc4b7f3d25e002127ba04a6129
Andrey Torkhov <torkhovasic@yandex.ru>
2024-10-30 21:06:03 +0300

    Created a hook to recalculate current values

--- src/components/NewsList/NewsList.tsx
+++ src/components/NewsList/NewsList.tsx
@@ -1,25 +1,134 @@
-import React, {
+import {
+  useCallback,
   useEffect,
-  useState,
-  useRef,
   useLayoutEffect,
   useMemo,
+  useRef,
+  useState,
 } from "react";
 // import NewsItem from "./NewsItem";
 import { fetchNews } from "../../api/gdeltApi";
 import { NewsItemType } from "../../types";
-import styles from "../../styles/NewsList.module.css";
+// import styles from "../../styles/NewsList.module.css";
+
+interface UseFixedSizeListProps {
+  loading: boolean;
+  itemsCount: number;
+  itemHeight: number;
+  listHeight: number;
+  overscan?: number;
+  scrollingDelay?: number;
+  getScrollElement: () => HTMLElement | null;
+}
+
+const DEFAULT_OVERSCAN = 3;
+const DEFAULT_SCROLLING_DELAY = 150;
+
+function useFixedSizeList(props: UseFixedSizeListProps) {
+  const {
+    loading,
+    itemHeight,
+    itemsCount,
+    scrollingDelay = DEFAULT_SCROLLING_DELAY,
+    overscan = DEFAULT_OVERSCAN,
+    listHeight,
+    getScrollElement,
+  } = props;
+
+  const [scrollTop, setScrollTop] = useState(0);
+  const [isScrolling, setIsScrolling] = useState(false);
+
+  useLayoutEffect(() => {
+    if (loading) return;
+    const scrollElement = getScrollElement();
+
+    if (!scrollElement) {
+      return;
+    }
+
+    const handleScroll = () => {
+      const scrollTop = scrollElement.scrollTop;
+
+      setScrollTop(scrollTop);
+    };
+
+    handleScroll();
+
+    scrollElement.addEventListener("scroll", handleScroll);
+
+    return () => scrollElement.removeEventListener("scroll", handleScroll);
+  }, [getScrollElement, loading]);
+
+  useEffect(() => {
+    if (loading) return;
+    const scrollElement = getScrollElement();
+
+    if (!scrollElement) {
+      return;
+    }
+
+    let timeoutId: number | null = null;
+
+    const handleScroll = () => {
+      setIsScrolling(true);
+
+      if (typeof timeoutId === "number") {
+        clearTimeout(timeoutId);
+      }
+
+      timeoutId = setTimeout(() => {
+        setIsScrolling(false);
+      }, scrollingDelay);
+    };
+
+    scrollElement.addEventListener("scroll", handleScroll);
+
+    return () => {
+      if (typeof timeoutId === "number") {
+        clearTimeout(timeoutId);
+      }
+      scrollElement.removeEventListener("scroll", handleScroll);
+    };
+  }, [getScrollElement, loading, scrollingDelay]);
+
+  const { virtualItems, startIndex, endIndex } = useMemo(() => {
+    const rangeStart = scrollTop;
+    const rangeEnd = scrollTop + listHeight;
+
+    let startIndex = Math.floor(rangeStart / itemHeight);
+    let endIndex = Math.ceil(rangeEnd / itemHeight);
+
+    startIndex = Math.max(0, startIndex - overscan);
+    endIndex = Math.min(itemsCount - 1, endIndex + overscan);
+
+    const virtualItems = [];
+
+    for (let index = startIndex; index <= endIndex; index++) {
+      virtualItems.push({
+        index,
+        offsetTop: index * itemHeight,
+      });
+    }
+    return { virtualItems, startIndex, endIndex };
+  }, [scrollTop, listHeight, itemsCount]);
+
+  const totalHeight = itemHeight * itemsCount;
+
+  return {
+    virtualItems,
+    totalHeight,
+    startIndex,
+    endIndex,
+    isScrolling,
+  };
+}
 
 const itemHeight = 50;
 const containerHeight = 600;
-const overscan = 3;
-const DEFAULT_SCROLLING_DELAY = 150;
 
 const NewsList: React.FC = () => {
   const [news, setNews] = useState<NewsItemType[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
-  const [scrollTop, setScrollTop] = useState(0);
-  const [isScrolling, setIsScrolling] = useState(false);
 
   const scrollElementRef = useRef<HTMLDivElement>(null);
 
@@ -39,108 +148,34 @@ const NewsList: React.FC = () => {
     loadNews();
   }, []);
 
-  useLayoutEffect(() => {
-    if (loading) return;
-    const scrollElement = scrollElementRef.current;
-
-    if (!scrollElement) {
-      return;
-    }
-
-    const handleScroll = () => {
-      const scrollTop = scrollElement.scrollTop;
-      setScrollTop(scrollTop);
-    };
-
-    handleScroll();
-
-    scrollElement.addEventListener("scroll", handleScroll);
-
-    return () => scrollElement.removeEventListener("scroll", handleScroll);
-  }, [loading]);
-
-  useEffect(() => {
-    if (loading) return;
-    const scrollElement = scrollElementRef.current;
-
-    if (!scrollElement) {
-      return;
-    }
-
-    let timeoutId: number | null = null;
-
-    const handleScroll = () => {
-      setIsScrolling(true);
-
-      if (typeof timeoutId === "number") {
-        clearTimeout(timeoutId);
-      }
-
-      timeoutId = setTimeout(() => {
-        setIsScrolling(false);
-      }, DEFAULT_SCROLLING_DELAY);
-    };
-
-    scrollElement.addEventListener("scroll", handleScroll);
-
-    return () => {
-      if (typeof timeoutId === "number") {
-        clearTimeout(timeoutId);
-      }
-      scrollElement.removeEventListener("scroll", handleScroll);
-    };
-  }, [loading]);
-
-  const virtualItems = useMemo(() => {
-    const rangeStart = scrollTop;
-    const rangeEnd = scrollTop + containerHeight;
-
-    let startIndex = Math.floor(rangeStart / itemHeight);
-    let endIndex = Math.ceil(rangeEnd / itemHeight);
-
-    startIndex = Math.max(0, startIndex - overscan);
-    endIndex = Math.min(news.length - 1, endIndex + overscan);
-
-    const virtualItems = [];
-    for (let index = startIndex; index <= endIndex; index++) {
-      virtualItems.push({
-        index,
-        offsetTop: index * itemHeight,
-      });
-    }
-
-    return virtualItems;
-  }, [scrollTop, news.length]);
-
-  //   const itemsToRender = news.slice(startIndex, endIndex + 1);
-  const totalListHight = itemHeight * news.length;
+  const { isScrolling, virtualItems, totalHeight } = useFixedSizeList({
+    loading: loading,
+    itemHeight: itemHeight,
+    itemsCount: news.length,
+    listHeight: containerHeight,
+    getScrollElement: useCallback(() => scrollElementRef.current, []),
+  });
 
   if (loading) return <p>Loading...</p>;
 
   return (
     <div
       ref={scrollElementRef}
-      className={styles.newsList}
       style={{
         height: containerHeight,
         overflow: "auto",
-        border: "1px solid red",
+        border: "1px solid lightgrey",
         position: "relative",
       }}
     >
       <div
         style={{
-          height: totalListHight,
+          height: totalHeight,
         }}
       >
         {virtualItems.map((virtualItem) => {
           const item = news[virtualItem.index]!;
           return (
-            // <NewsItem
-            //   key={virtualItem.index}
-            //   news={item}
-            //   index={virtualItem.index}
-            // />
             <div
               style={{
                 position: "absolute",
